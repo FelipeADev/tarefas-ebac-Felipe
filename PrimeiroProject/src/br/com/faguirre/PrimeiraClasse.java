cd80f342d2168806a77020b83e24b70f363e9a89
Felipe Aguirre <exemplo@teste.com.br>
2023-07-21 16:34:38 -0300

    Initial commit

---
+++ PrimeiroProject/src/br/com/faguirre/PrimeiraClasse.java
@@ -0,0 +1,15 @@
+package br.com.faguirre;
+
+public class PrimeiraClasse {
+
+	public static void main(String args[]) {
+		String hello = args[0];
+		
+		if (hello.equals("Felipe")){
+			System.out.print("Hello Felipe");
+		}
+				
+
+	}
+
+}
